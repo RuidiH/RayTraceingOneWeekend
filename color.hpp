bb160d75cecfa842b603a756cf2f0b0e11c4b536
RuidiH <ruidihuang@mymail.mines.edu>
2025-03-25 19:59:47 -0700

    add sampling technique

--- color.hpp
+++ color.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "vec3.hpp"
+#include "interval.hpp"
 
 using color = vec3;
 
@@ -8,10 +9,12 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
-
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+   
+    // translate from [0, 1] to [0, 255]
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     out << rbyte << ' ' << gbyte  << ' ' << bbyte << '\n';
 }
