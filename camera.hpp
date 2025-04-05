60315518abf1d32bf9145ed8d5ce96c55472ae91
RuidiH <ruidihuang@mymail.mines.edu>
2025-04-05 00:16:08 -0700

    add fov to camera

--- camera.hpp
+++ camera.hpp
@@ -11,6 +11,11 @@ public:
     int samples_per_pixel = 10; // random samples count for each pixel
     int max_depth = 10;         // max num of ray bounce in scene
 
+    double vfov = 90;                  // vertical view angle (fov)
+    point3 lookfrom = point3(0, 0, 0); // point camera is looking from
+    point3 lookat = point3(0, 0, -1);  // point camera is looking at
+    vec3 vup = vec3(0, 1, 0);          // camera-relative "up" direction
+
     void render(const hittable &world)
     {
         initialize();
@@ -44,6 +49,7 @@ private:
     point3 pixel00_loc;         // location of pixel 0,0 at top left
     vec3 pixel_delta_u;         // offset to pixel to right
     vec3 pixel_delta_v;         // offset to pixel below
+    vec3 u, v, w;               // camera frame basis vectors
 
     void initialize()
     {
@@ -52,22 +58,30 @@ private:
 
         pixel_samples_scale = 1.0 / samples_per_pixel;
 
-        center = point3(0, 0, 0);
+        center = lookfrom;
 
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        // Determin viewport dimension
+        auto focal_length = (lookfrom - lookat).length();
+        auto theta = degrees_to_radians(vfov);
+        auto h = std::tan(theta / 2);
+        auto viewport_height = 2 * h * focal_length;
         auto viewport_width = viewport_height * (double(image_width) / image_height);
 
-        // Calculate Cam u,v on viewport plane
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        // Calculate u,v,w unit basis vectors for the camera coordinate frame.
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        auto viewport_u = viewport_width * u;   // Vector across viewport horizontal edge
+        auto viewport_v = viewport_height * -v;  // Vector down viewport vertical edge
 
         // Calculate u, v delta
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate upper-left pixel
-        auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
@@ -106,7 +120,7 @@ private:
             ray scattered;
             color attenuation;
             if (rec.mat->scatter(r, rec, attenuation, scattered))
-                return attenuation * ray_color(scattered, depth-1, world);
+                return attenuation * ray_color(scattered, depth - 1, world);
             return color(0, 0, 0);
         }
 
